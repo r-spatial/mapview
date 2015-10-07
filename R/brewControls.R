1b964110eaa6514b8c60dd38fdc9fb0d9aaf234f
tim-salabim <tim.appelhans@gmail.com>
2015-10-07 17:37:35 +0200

    readded brew functionality for speed comparison to whisker

---
+++ R/brewControls.R
@@ -0,0 +1,93 @@
+#' Convenience functions for brew plugin
+#'
+#' @author
+#' Tim Appelhans
+#'
+#' @name brewControls
+NULL
+
+
+# brew popup table for sp objects -----------------------------------------
+#' @describeIn brewControls create popup table for sp objects
+brewPopupTable <- function(x) {
+  df <- as.data.frame(sapply(x@data, as.character),
+                      stringsAsFactors = FALSE)
+
+  if (nrow(x) == 1) df <- t(df)
+
+  df$x <- as.character(round(coordinates(x)[, 1], 2))
+  df$y <- as.character(round(coordinates(x)[, 2], 2))
+
+  cols <- colnames(df)
+
+  vals <- sapply(seq(nrow(x@data)), function(i) {
+    df[i, ]
+  })
+
+
+  indodd <- seq(1, ncol(df), by = 2)
+  indeven <- seq(2, ncol(df), by = 2)
+
+  txt <- lapply(seq(nrow(df)), function(j) {
+    odd <- sapply(indodd, function(i) {
+      brewPopupRow(cols[i], df[j, i])
+    })
+
+    even <- sapply(indeven, function(i) {
+      brewPopupRowAlt(cols[i], df[j, i])
+    })
+
+    pop <- vector("character", length(cols))
+
+    pop[indodd] <- odd
+    pop[indeven] <- even
+
+    popTemplate <- system.file("templates/popup.brew", package = "mapview")
+    myCon <- textConnection("outputObj", open = "w")
+    brew::brew(popTemplate, output = myCon)
+    outputObj <- outputObj
+    close(myCon)
+
+    return(paste(outputObj, collapse = ' '))
+  })
+  return(txt)
+  print(txt)
+}
+
+
+# create popup table odd row for sp objects ---------------------------------------
+#' @describeIn brewControls create popup table odd row for sp objects
+#'
+#' @param col.name the name of the column of the attribute table
+#' @param value the corresponding value from the attribute table
+#'
+brewPopupRow <- function(col.name, value) {
+
+  paste0("<tr>",
+         paste0("<td>",
+                col.name,
+                "</td>"),
+         paste0("<td>",
+                value,
+                "</td>"),
+         "</tr>")
+
+}
+
+
+# create popup table even row for sp objects ---------------------------------------
+#' @describeIn brewControls create popup table even row for sp objects
+#'
+brewPopupRowAlt <- function(col.name, value) {
+
+  paste0("<tr class='alt'>",
+         paste0("<td>",
+                col.name,
+                "</td>"),
+         paste0("<td>",
+                value,
+                "</td>"),
+         "</tr>")
+
+}
+
--- R/mapView.R
+++ R/mapView.R
@@ -378,7 +378,7 @@ setMethod('mapView', signature(x = 'SpatialPointsDataFrame'),
 
                 x <- lst[[i]]
 
-                if (pop.null) popup <- createPopupTable(x)
+                if (pop.null) popup <- brewPopupTable(x)
 
                 m <- leaflet::addCircleMarkers(m,
                                                lng = coordinates(x)[, 1],
@@ -410,7 +410,7 @@ setMethod('mapView', signature(x = 'SpatialPointsDataFrame'),
 
               grp <- layer.name
 
-              if (pop.null) popup <- createPopupTable(x)
+              if (pop.null) popup <- brewPopupTable(x)
 
               m <- leaflet::addCircleMarkers(map = m,
                                              lng = coordinates(x)[, 1],
@@ -555,7 +555,7 @@ setMethod('mapView', signature(x = 'SpatialPolygonsDataFrame'),
 
                 grp <- names(df)
 
-                if (pop.null) popup <- createPopupTable(x)
+                if (pop.null) popup <- brewPopupTable(x)
 
                 clrs <- pal_n[[i]](vals[[i]])
                 m <- leaflet::addPolygons(m,
@@ -588,7 +588,7 @@ setMethod('mapView', signature(x = 'SpatialPolygonsDataFrame'),
 
               grp <- layer.name
 
-              if (pop.null) popup <- createPopupTable(x)
+              if (pop.null) popup <- brewPopupTable(x)
 
               m <- leaflet::addPolygons(m,
                                         weight = weight,
