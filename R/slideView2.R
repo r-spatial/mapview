55e051c19bdd08659ff5814d3fde774a39db5d1e
swoellauer <swoellauer>
2015-10-14 11:24:19 +0200

    slideView2: basic functionality

--- R/slideView2.R
+++ R/slideView2.R
@@ -47,6 +47,43 @@ setMethod("slideView2", signature(img1 = "RasterStackBrick",
           function(img1, img2,
                    maxpixels = 500000) {
 
+            png1 <- rgbStack2PNG(img1, maxpixels = maxpixels)
+            png2 <- rgbStack2PNG(img2, maxpixels = maxpixels)
+
+            ## temp dir
+            dir <- tempfile()
+            dir.create(dir)
+            fl1 <- paste0(dir, "/img1", ".png")
+            fl2 <- paste0(dir, "/img2", ".png")
+
+            ## d3
+            d3_pth <- system.file("d3", package = "mapview")
+            d3_fls <- list.files(d3_pth, pattern = glob2rx("*.js"),
+                                 full.names = TRUE)
+            tmp <- file.copy(d3_fls, dir)
+
+            ## template
+            tmplt_pth <- system.file("d3/templates", package = "mapview")
+            tmplt_fl <- list.files(tmplt_pth, pattern = "slider.html",
+                                   full.names = TRUE)
+            tmplt <- paste(readLines(tmplt_fl), collapse = "\n")
+            htmlFile <- file.path(dir, "index.html")
+            write(tmplt, htmlFile)
+
+            ## pngs
+            png::writePNG(png1, fl1)
+            png::writePNG(png2, fl2)
+
+            ## view
+            #viewer <- getOption("viewer")
+            #if (!is.null(viewer))
+            #  viewer(htmlFile)
+            #else
+            #  utils::browseURL(htmlFile)
+
+            slideView2internal(list(a="a", b="b"), filename1 = fl1, filename2 = fl2)
+
+
           }
 
 )
@@ -61,7 +98,45 @@ setMethod("slideView2", signature(img1 = "RasterLayer",
                    colors = mapViewPalette(7),
                    na.color = "#00000000",
                    maxpixels = 500000) {
+            png1 <- raster2PNG(img1, colors = colors,
+                               na.color = na.color,
+                               maxpixels = maxpixels)
+            png2 <- raster2PNG(img2, colors = colors,
+                               na.color = na.color,
+                               maxpixels = maxpixels)
 
+            ## temp dir
+            dir <- tempfile()
+            dir.create(dir)
+            fl1 <- paste0(dir, "/img1", ".png")
+            fl2 <- paste0(dir, "/img2", ".png")
+
+            ## d3
+            d3_pth <- system.file("d3", package = "mapview")
+            d3_fls <- list.files(d3_pth, pattern = glob2rx("*.js"),
+                                 full.names = TRUE)
+            tmp <- file.copy(d3_fls, dir)
+
+            ## template
+            tmplt_pth <- system.file("d3/templates", package = "mapview")
+            tmplt_fl <- list.files(tmplt_pth, pattern = "slider.html",
+                                   full.names = TRUE)
+            tmplt <- paste(readLines(tmplt_fl), collapse = "\n")
+            htmlFile <- file.path(dir, "index.html")
+            write(tmplt, htmlFile)
+
+            ## pngs
+            png::writePNG(png1, fl1)
+            png::writePNG(png2, fl2)
+
+            ## view
+            #viewer <- getOption("viewer")
+            #if (!is.null(viewer))
+            #  viewer(htmlFile)
+            #else
+            #  utils::browseURL(htmlFile)
+
+            slideView2internal(list(a="a", b="b"), filename1 = fl1, filename2 = fl2)
 
           }
 
@@ -75,13 +150,45 @@ setMethod("slideView2", signature(img1 = "character",
                                   img2 = "character"),
           function(img1, img2) {
 
+            png1 <- png::readPNG(img1)
+            png2 <- png::readPNG(img2)
+
+            ## temp dir
+            dir <- tempfile()
+            dir.create(dir)
+            fl1 <- paste0(dir, "/img1", ".png")
+            fl2 <- paste0(dir, "/img2", ".png")
+
+            ## d3
+            d3_pth <- system.file("d3", package = "mapview")
+            d3_fls <- list.files(d3_pth, pattern = glob2rx("*.js"),
+                                 full.names = TRUE)
+            tmp <- file.copy(d3_fls, dir)
+
+            ## template
+            tmplt_pth <- system.file("d3/templates", package = "mapview")
+            tmplt_fl <- list.files(tmplt_pth, pattern = "slider.html",
+                                   full.names = TRUE)
+            tmplt <- paste(readLines(tmplt_fl), collapse = "\n")
+            htmlFile <- file.path(dir, "index.html")
+            write(tmplt, htmlFile)
+
+            ## pngs
+            png::writePNG(png1, fl1)
+            png::writePNG(png2, fl2)
+
+            ## view
+            #viewer <- getOption("viewer")
+            #if (!is.null(viewer))
+            #  viewer(htmlFile)
+            #else
+            #  utils::browseURL(htmlFile)
+
+            slideView2internal(list(a="a", b="b"), filename1 = fl1, filename2 = fl2)
 
           }
 
 )
-
-
-
 
 #' <Add Title>
 #'
@@ -90,16 +197,18 @@ setMethod("slideView2", signature(img1 = "character",
 #' @import htmlwidgets
 #'
 #' @export
-slideView2internal <- function(message, width = NULL, height = NULL) {
+slideView2internal <- function(message, width = NULL, height = NULL, filename1 = NULL, filename2 = NULL) {
 
   # forward options using x
   x = list(
     message = message
   )
 
-  test_dir = "c:/temp2"
-  test_file1 = "test1.png"
-  test_file2 = "test2.png"
+  #filename1 and filename2 need to have same directory!
+  test_dir = dirname(filename1)
+
+  test_file1 = basename(filename1)
+  test_file2 = basename(filename2)
 
   dep1 <- htmltools::htmlDependency(name="test", version="1", src = c(file=test_dir), attachment = list(test_file1, test_file2) )
   deps <- list(dep1)
