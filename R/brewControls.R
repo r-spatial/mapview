0c656d2c63a3016e05b1040dc3a0c5fd0e571743
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-10-22 17:19:50 +0200

    Added function to create html template based upon 'Rcpp'.

--- R/brewControls.R
+++ R/brewControls.R
@@ -1,7 +1,7 @@
 #' Convenience functions for brew plugin
 #'
 #' @author
-#' Tim Appelhans
+#' Tim Appelhans, Florian Detsch
 #'
 #' @name brewControls
 NULL
@@ -10,8 +10,10 @@ NULL
 # brew popup table for sp objects -----------------------------------------
 #' @describeIn brewControls create popup table for sp objects
 brewPopupTable <- function(x) {
-  df <- as.data.frame(sapply(x@data, as.character),
-                      stringsAsFactors = FALSE)
+  # df <- as.data.frame(sapply(x@data, as.character),
+  #                     stringsAsFactors = FALSE)
+  df <- data.frame(mapview:::df2String(x@data), stringsAsFactors = FALSE)
+  names(df) <- names(x@data)
 
   if (nrow(x) == 1) df <- t(df)
 
@@ -20,35 +22,40 @@ brewPopupTable <- function(x) {
 
   cols <- colnames(df)
 
-  vals <- sapply(seq(nrow(x@data)), function(i) {
-    df[i, ]
-  })
-
+  # vals <- sapply(seq(nrow(x@data)), function(i) {
+  #   df[i, ]
+  # })
 
   indodd <- seq(1, ncol(df), by = 2)
   indeven <- seq(2, ncol(df), by = 2)
 
   txt <- lapply(seq(nrow(df)), function(j) {
-    odd <- sapply(indodd, function(i) {
-      brewPopupRow(cols[i], df[j, i])
-    })
+    #     odd <- sapply(indodd, function(i) {
+    #       brewPopupRow(cols[i], df[j, i])
+    #     })
+    #
+    #     even <- sapply(indeven, function(i) {
+    #       brewPopupRowAlt(cols[i], df[j, i])
+    #     })
+    #
+    #     pop <- vector("character", length(cols))
+    #
+    #     pop[indodd] <- odd
+    #     pop[indeven] <- even
+    #
+    #     mapview:::mergePopupRows(cols, as.character(df[j, ]))
+    #
+    #     popTemplate <- system.file("templates/popup.brew", package = "mapview")
+    #     myCon <- textConnection("outputObj", open = "w")
+    #     brew::brew(popTemplate, output = myCon)
+    #     outputObj <- outputObj
+    #     close(myCon)
+    #
+    #     mapview:::htmlTemplate(cols, unlist(df[j, ]))
+    #
+    #     return(paste(outputObj, collapse = ' '))
 
-    even <- sapply(indeven, function(i) {
-      brewPopupRowAlt(cols[i], df[j, i])
-    })
-
-    pop <- vector("character", length(cols))
-
-    pop[indodd] <- odd
-    pop[indeven] <- even
-
-    popTemplate <- system.file("templates/popup.brew", package = "mapview")
-    myCon <- textConnection("outputObj", open = "w")
-    brew::brew(popTemplate, output = myCon)
-    outputObj <- outputObj
-    close(myCon)
-
-    return(paste(outputObj, collapse = ' '))
+    mapview:::htmlTemplate(cols, unlist(df[j, ]))
   })
   return(txt)
   print(txt)
