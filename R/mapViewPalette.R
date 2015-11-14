e17e8830aae87be51f80be6cc75e1f2f8084e526
tim-salabim <tim.appelhans@gmail.com>
2015-11-14 22:43:43 +0100

    minor cleaning of mapViewPalette

--- R/mapViewPalette.R
+++ R/mapViewPalette.R
@@ -11,13 +11,12 @@
 #' @name mapViewPalette
 #' @export mapViewPalette
 #' @aliases mapViewPalette
-NULL
 
 mapViewPalette <- function(n) {
-  colorRampPalette(c("#ebeaf7", "#83b0d6",
-                     "#55A1B1", "#63AD99",
-                     "#7FB972", "#B5BD4C",
-                     "#D9AD3C", "#E68E34",
-                     "#E6642C", "#D92120",
-                     "#460000"))(n)
+  grDevices::colorRampPalette(c("#ebeaf7", "#83b0d6",
+                                "#55A1B1", "#63AD99",
+                                "#7FB972", "#B5BD4C",
+                                "#D9AD3C", "#E68E34",
+                                "#E6642C", "#D92120",
+                                "#460000"))(n)
 }
