e48aa84b816685ec9d3faa1438ad3063ddff1c66
swoellauer <swoellauer>
2016-02-23 09:29:46 +0100

    cubeView example

--- testing/testing_cubeView.R
+++ testing/testing_cubeView.R
@@ -11,3 +11,15 @@ g <- c(0:(xyz_size-1))/7
 b <- c(0:(xyz_size-1))/1
 
 cubeView(red=r, green=g, blue=b, x_size=x_size, y_size=y_size, z_size=z_size)
+
+
+
+p <- poppendorf
+v <- as.vector(p@data@values)
+v <- v / (max(v)/255)
+
+x_size <- p@ncols
+y_size <- p@nrows
+z_size <- p@data@nlayers
+
+cubeView(red=v, green=v, blue=v, x_size=x_size, y_size=y_size, z_size=z_size)
