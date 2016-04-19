bb056d8a0f62735b0fa48a631b1f5a977424f467
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-04-19 10:52:13 +0200

    Initial upload of function `popupTable`.

---
+++ R/popupTable.R
@@ -0,0 +1,34 @@
+#' Create mapview popup table
+#'
+#' @description
+#' Create a \code{popup} table used as input for \code{\link{mapview}}. This
+#' optionally allows the user to include only a subset of feature attributes.
+#'
+#' @param x \code{Spatial*} or \code{Raster*} object.
+#' @param zcol \code{numeric} or \code{character} vector indicating the columns
+#' included in the output popup table. If missing, all columns are displayed.
+#' @param use_cpp \code{logical} determining whether or not to enable
+#' \strong{Rcpp} functionality.
+#'
+#' @return
+#' A \code{list} of HTML strings required to create feature popup table(s).
+#'
+#' @examples
+#' library(sp)
+#'
+#' data(meuse)
+#' coordinates(meuse) <- ~ x + y
+#' proj4string(meuse) <- CRS("+init=epsg:28992")
+#'
+#' ## include columns 1 and 2 only
+#' mapview(meuse, popup = popupTable(meuse, zcol = 1:2))
+#'
+#' @export popupTable
+#' @name popupTable
+popupTable <- function(x, zcol, use_cpp = TRUE) {
+
+  if (!missing(zcol))
+    x <- x[, zcol]
+
+  brewPopupTable(x, use_cpp = use_cpp)
+}
