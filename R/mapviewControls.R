993f5fe9612a79b323f4cc8565da8fb576ce220e
tim-salabim <tim.appelhans@gmail.com>
2016-04-19 12:44:13 +0200

    added burst function to burst by rows

--- R/mapviewControls.R
+++ R/mapviewControls.R
@@ -25,3 +25,29 @@ getSimpleClass <- function(obj) {
 makeLabels <- function(col) {
   as.character(col)
 }
+
+
+### burst
+burst <- function(x, zcol, ...) {
+
+  lst <- lapply(seq(zcol), function(i) {
+    if (!is.factor(x@data[, zcol[i]])) {
+      x@data[, zcol[i]] <- as.factor(x@data[, zcol[i]])
+    }
+    f <- x@data[, zcol[i]]
+    ls_out <- split(x[, zcol[i]], f, ...)
+    names(ls_out) <- paste(zcol[i], names(ls_out), sep = "_")
+    return(ls_out)
+  })
+
+  names(lst) <- zcol
+
+  if (length(lst) > 1) {
+    ls <- lst[[1]]
+    for (i in 2:length(lst)) ls <- append(ls, lst[[i]])
+  } else {
+    ls <- lst[[1]]
+  }
+
+  return(ls)
+}
