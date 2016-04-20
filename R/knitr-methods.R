e1d7deba0a65e9d51d7c1e0e8c15487dd4afcc33
tim-salabim <tim.appelhans@gmail.com>
2016-04-20 14:01:38 +0200

    added shiny functionality

--- R/knitr-methods.R
+++ R/knitr-methods.R
@@ -12,9 +12,26 @@
 #' @export
 #'
 knit_print.mapview <- function(x, ..., options = NULL) {
-
   knitr::knit_print(htmlwidgets:::toHTML(mapview2leaflet(x),
                                          standalone = FALSE,
                                          knitrOptions = options),
                     options = options,  ...)
+}
+
+
+#' @export
+#'
+renderMapview <- function (expr, env = parent.frame(), quoted = FALSE) {
+  expr <- expr@map
+  if (!quoted)
+    expr = substitute(expr)
+  htmlwidgets::shinyRenderWidget(expr, leafletOutput, env,
+                                 quoted = TRUE)
+}
+
+#' @export
+#'
+mapviewOutput <- function (outputId, width = "100%", height = 400) {
+  htmlwidgets::shinyWidgetOutput(outputId, "leaflet", width,
+                                 height, "leaflet")
 }
