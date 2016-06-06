aa67d3bf831e3caafbae4e51d7f1e82dac92c39f
tim-salabim <tim.appelhans@gmail.com>
2016-06-06 20:03:45 +0200

    deleted projview related data entries

--- R/mapview-package.R
+++ R/mapview-package.R
@@ -79,19 +79,3 @@ NULL
 #' "The Geography of Beer, sustainability in the food industry"
 #' @format \code{sp::SpatialPointsDataFrame}
 NULL
-#'
-#' @docType data
-#' @name campsQ2
-#' @title Antarctic Facilities
-#' @description Antarctic facilities. Type of facility attribute is ""feature_ty"
-#' @details The data show the Antarctic facilities (2012) as provided by \href{https://www.comnap.aq}{comnap} via the \href{www.quantarctica.org}{Quantarctica2} data compilation that is compiled by the \href{http://www.npolar.no/en}{Norwegian Polar Institute}
-#' @format \code{sp::SpatialPointsDataFrame}
-NULL
-#'
-#' @docType data
-#' @name roadsGRL
-#' @title roads data from Greenland as provided by geofabrik.de
-#' @description Roads snapshot March(2016) from the OSM data extract as provided by geofabrik.de
-#' @details The data show the roads (snapshot 03/2013) as downloaded by \href{http://download.geofabrik.de/north-america/greenland-latest.shp.zip}{geofabrik}. The geofabrik extracts are based on the OpenStreetMap \href{http://www.openstreetmap.org/}{OSM} data. For further information see also \href{http://download.geofabrik.de/north-america/greenland.html}{Geofabrik Downloads}
-#' @format \code{sp::SpatialPointsDataFrame}
-NULL
