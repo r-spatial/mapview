312ff27a17891d62560ff739beb0e1209959a798
tim-salabim <tim.appelhans@gmail.com>
2016-06-19 19:53:06 +0200

    first integration of sortableLayersControl. not working! seems to be a problem of correctly identifying the layers (pane...)

---
+++ R/sortableLayersControl.R
@@ -0,0 +1,52 @@
+sortableLayersControlDependencies <- function() {
+  list(
+    htmltools::htmlDependency(
+      "sortableLayersControl",
+      '0.0.1',
+      system.file("htmlwidgets/lib/sortable_layers_control", package = "mapview"),
+      script = c("sortableLayersControl.js"),
+      stylesheet = 'sortableLayersControl.css'
+    ))
+}
+
+
+#
+# # ' Add full screen control to map
+# # '
+# # ' Add full screen control (https://github.com/brunob/leaflet.fullscreen)
+# # ' @param map the map to add the tile layer to
+# # ' @return modified map object
+# # '
+# # ' @examples
+# # ' leaflet() %>%
+# # '   addControlFullScreen()
+# # ' @export
+# addEasyButton <- function(
+#   map
+# ) {
+#   map$dependencies <- c(map$dependencies, leafletFullScreenDependencies())
+#   invokeMethod(map, leaflet::getMapData(map), 'addControlFullScreen')
+# }
+
+sortableLayersControl <- function(map, layer.names) {
+
+  li <- do.call(paste0, lapply(seq(layer.names), function(i) {
+    paste(
+      "<li id=", layer.names[[i]],
+      "><input type='checkbox' checked>",
+      layer.names[[i]], "</li>", sep = "")
+  }))
+
+  inner_html <- paste(
+    "<div class='layer block'><div class='layer title'><strong>Layers</strong>",
+    "</div><ul id='layer_list' class='block__list block__list_words'>",
+    li,
+    "</ul></div>")
+
+  map$dependencies <- c(map$dependencies, sortableLayersControlDependencies())
+  invokeMethod(map, leaflet::getMapData(map), 'sortableLayersControl',
+               inner_html)
+
+}
+
+
---
+++ inst/htmlwidgets/lib/sortable_layers_control/sortableLayersControl.js
@@ -0,0 +1,65 @@
+LeafletWidget.methods.sortableLayersControl = function (inner_html) {
+
+  L.ControlSort = L.Control.extend({
+  options: {
+    position: 'topleft',
+  },
+  onAdd: function() {
+    var sortableDiv = L.DomUtil.create('div', 'sortableContainer', this._container);
+    sortableDiv.innerHTML = inner_html;
+    return sortableDiv;
+  }
+});
+
+L.controlSort = function(options) {
+  return new L.ControlSort(options);
+};
+
+L.controlSort().addTo(this);
+
+var map = this;
+
+layerList = document.getElementById('layer_list');
+
+var layArr = getIds(layerList);
+sortLayers(layArr);
+
+Sortable.create(layerList, {
+  animation: 150,
+  onUpdate: function(evt) {
+    layArr = getIds(layerList);
+    sortLayers(layArr);
+  }
+});
+
+function sortLayers(layArr) {
+  for (var i = 0; i < layArr.length; i++) {
+    map.createPane(layArr[i]);
+  	map.getPane(layArr[i]).style.zIndex = 400 - i;
+  }
+}
+
+function getIds(inputList) {
+  var outputArr = [];
+  for (var i = 0; i < inputList.children.length; i++) {
+    outputArr[i] = inputList.children[i].id;
+  }
+  return outputArr;
+}
+
+L.DomEvent.disableClickPropagation(layerList);
+
+$( "input" ).change(function( event ) {
+    layerClicked = eval(event.target.value);
+    if (map.hasLayer(layerClicked)) {
+       map.removeLayer(layerClicked);
+    }
+    else{
+       map.addLayer(layerClicked);
+    } ;
+ });
+
+
+};
+
+
