1682e421ee1285e243b866ffec90f634418c51bf
tim-salabim <tim.appelhans@gmail.com>
2017-03-11 22:54:58 +0100

    more bugfixing of burstByRow

--- R/burst.R
+++ R/burst.R
@@ -2,6 +2,8 @@ burst <- function(x,
                   zcol,
                   burst,
                   color,
+                  at,
+                  na.color,
                   popup,
                   ...) {
 
@@ -13,9 +15,18 @@ burst <- function(x,
   if (is.null(zcol) & !burst) {
     x
   } else if (is.null(zcol) & burst) {
-    function() burstByColumn(x = x, color = color, popup = popup)
-  } else {
-    function() burstByRow(x = x, zcol = zcol, burst = burst, color = color)
+    function() burstByColumn(x = x,
+                             color = color,
+                             at = at,
+                             na.color = na.color,
+                             popup = popup)
+  } else if (!is.null(zcol) & burst) {
+    function() burstByRow(x = x,
+                          zcol = zcol,
+                          burst = burst,
+                          color = color,
+                          at = at,
+                          na.color = na.color)
   }
 
 }
@@ -23,6 +34,8 @@ burst <- function(x,
 
 burstByColumn <- function(x,
                           color,
+                          at,
+                          na.color,
                           popup = popupTable(x),
                           ...) {
 
@@ -32,8 +45,10 @@ burstByColumn <- function(x,
   })
   names(x_lst) <- nms
 
+  popup <- rep(list(popup), length(x_lst))
+
   color_lst <- lapply(nms, function(i) {
-    vectorColors(x, zcol = i, color = color)
+    vectorColors(x, zcol = i, colors = color, at = at, na.color = na.color)
   })
 
   labs <- lapply(nms, function(i) {
@@ -54,18 +69,35 @@ burstByRow <- function(x,
                        zcol,
                        burst,
                        color,
+                       at,
+                       na.color,
                        ...) {
-
-  popup <- popupTable(x)
-  color <- as.list(vectorColors(x, zcol, color))
 
   x[[zcol]] <- as.character(x[[zcol]])
   x[[zcol]][is.na(x[[zcol]])] <- "NA"
 
-  x <- x[, zcol, drop = FALSE]
-  lst <- split(x, x[[zcol]])[as.character(x[[zcol]])]
+  lst <- split(x, x[[zcol]], drop = TRUE) #[as.character(x[[zcol]])]
+
+  vec <- names(lst)
+  vec[vec == "NA"] <- NA
+
+  color <- as.list(zcolColors(x = vec,
+                              colors = color,
+                              at = at,
+                              na.color = na.color))
+  names(color) <- names(lst)
+
+  popup <- popupTable(x)
+  names(popup) <- as.character(x[[zcol]])
+  popup <- lapply(names(lst), function(i) {
+    tst <- popup[names(popup) %in% i]
+    names(tst) <- NULL
+    return(tst)
+  })
 
   labs <- lapply(lst, makeLabels, zcol = zcol)
+
+  # x <- x[, zcol, drop = FALSE]
 
   return(list(obj = lst,
               color = color,
