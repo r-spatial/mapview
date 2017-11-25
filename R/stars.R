f692478da443ea17cfb6c526dd086b2652584254
tim-salabim <tim.appelhans@gmail.com>
2017-11-25 13:12:09 +0100

    add data dependency to stars

--- R/stars.R
+++ R/stars.R
@@ -1,17 +1,16 @@
-addStars <- function(
-  map,
-  x,
-  bounds,
-  colors = "Spectral",
-  opacity = 1,
-  attribution = NULL,
-  layerId = NULL,
-  group = NULL,
-  project = FALSE,
-  maxBytes = 4*1024*1024
-) {
+addStarsImage <- function(map,
+                          x,
+                          bounds,
+                          colors = "Spectral",
+                          opacity = 1,
+                          attribution = NULL,
+                          layerId = NULL,
+                          group = NULL,
+                          project = FALSE,
+                          maxBytes = 4*1024*1024) {
   stopifnot(inherits(x, "stars"))
 
+  if (is.null(group)) group = "stars"
   # if (project) {
   #   projected <- projectRasterForLeaflet(x)
   # } else {
@@ -19,8 +18,24 @@ addStars <- function(
   # }
   # bounds <- raster::extent(raster::projectExtent(raster::projectExtent(x, crs = sp::CRS(epsg3857)), crs = sp::CRS(epsg4326)))
 
+  jsgroup <- gsub(".", "", make.names(group), fixed = TRUE)
+
+  tmp <- makepathStars(as.character(jsgroup))
+  pathDatFn <- tmp[[2]][1]
+  starspathDatFn <- tmp[[3]][1]
+  datFn <- tmp[[4]][1]
+
+  pre <- paste0('var ', jsgroup, ' = ')
+  writeLines(pre, pathDatFn)
+  # gj <- paste(pre, geojsonio::geojson_json(data), ';', sep = "\n")
+  txt = as.character(gsub("NA", "NaN", format(projected[[1]])))
+  cat(txt, file = pathDatFn, append = TRUE)
+  # file.append(pathDatFn, starspathDatFn)
+  # file.remove(starspathDatFn)
+
   if (!is.function(colors)) {
-    colors <- colorNumeric(colors, domain = NULL, na.color = "#00000000", alpha = TRUE)
+    colors <- colorNumeric(colors, domain = NULL,
+                           na.color = "#00000000", alpha = TRUE)
   }
 
   tileData <- as.numeric(projected[[1]]) %>%
@@ -28,7 +43,11 @@ addStars <- function(
   dim(tileData) <- c(4, nrow(projected), ncol(projected))
   pngData <- png::writePNG(tileData)
   if (length(pngData) > maxBytes) {
-    stop("Raster image too large; ", length(pngData), " bytes is greater than maximum ", maxBytes, " bytes")
+    stop("Raster image too large; ",
+         length(pngData),
+         " bytes is greater than maximum ",
+         maxBytes,
+         " bytes")
   }
   encoded <- base64enc::base64encode(pngData)
   uri <- paste0("data:image/png;base64,", encoded)
@@ -38,6 +57,44 @@ addStars <- function(
     list(raster::ymin(bounds), raster::xmax(bounds))
   )
 
-  invokeMethod(map, getMapData(map), "addRasterImage", uri, latlng, opacity, attribution, layerId, group) %>%
-    expandLimits(c(raster::ymin(bounds), raster::ymax(bounds)), c(raster::xmin(bounds), raster::xmax(bounds)))
+  map$dependencies <- c(map$dependencies,
+                        starsDataDependency(jFn = pathDatFn,
+                                            counter = 1,
+                                            group = jsgroup))
+
+  invokeMethod(map, getMapData(map), "addRasterImage", uri, latlng, opacity,
+               attribution, layerId, group) %>%
+    expandLimits(c(raster::ymin(bounds), raster::ymax(bounds)),
+                 c(raster::xmin(bounds), raster::xmax(bounds)))
+}
+
+
+
+makepathStars <- function(group) {
+  dirs <- list.dirs(tempdir())
+  # tmpPath <- grep(utils::glob2rx("*data_large*"), dirs, value = TRUE)
+  # if (length(tmpPath) == 0) {
+  tmpPath <- paste(tempfile(pattern = "data_stars"),
+                   createFileId(),
+                   sep = "_")
+  dir.create(tmpPath)
+  # }
+  baseFn <- paste("data_stars", group, sep = "_")
+  extFn <- "txt"
+  datFn <- paste0(baseFn, createFileId(), ".", extFn)
+  pathDatFn <- paste0(tmpPath, "/", datFn)
+  starspathDatFn <- paste0(tmpPath, "/", "stars_", datFn)
+  return(list(tmpPath, pathDatFn, starspathDatFn, datFn))
+}
+
+
+starsDataDependency <- function(jFn, counter = 1, group) {
+  data_dir <- dirname(jFn)
+  data_file <- basename(jFn)
+  list(
+    htmltools::htmlDependency(
+      name = group,
+      version = counter,
+      src = c(file = data_dir),
+      script = list(data_file)))
 }
