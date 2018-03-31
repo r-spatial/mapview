ef038703aec4c1b7d4788eb58b0ea95c27937fa4
pat-s <patrick.schratz@gmail.com>
2018-03-31 19:30:24 +0200

    lwgeom with internal compilation

--- tic.R
+++ tic.R
@@ -1,5 +1,9 @@
 add_package_checks()
 
+get_stage("install") %>%
+  # install lwgeom with its own library since linking again postgis source install fails sometimes
+  add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
+
 ###
 # deploy pkgdowm site
 ###
