f1b35e238d2ed05b73cfa0029d5751ad41fff0d8
pat-s <patrick.schratz@gmail.com>
2018-08-12 21:11:50 +0200

    remove orphan arg from step_push_deploy()

--- tic.R
+++ tic.R
@@ -16,5 +16,5 @@ if (Sys.getenv("id_rsa") != "") {
 
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
