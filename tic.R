f3285c4a6ff49748ead8a271b9a5ad5ed563f88e
pat-s <patrick.schratz@gmail.com>
2019-02-04 20:39:52 +0100

    update ci

--- tic.R
+++ tic.R
@@ -1,11 +1,10 @@
-add_package_checks(warnings_are_errors = FALSE)
+add_package_checks(error_on = "error")
 
 get_stage("install") %>%
   # install lwgeom with its own library since linking again postgis source install fails sometimes
   add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom")) %>% # normal installation does not work
   add_code_step(remotes::install_github("r-spatial/stars")) %>%
   add_code_step(remotes::install_github("hrbrmstr/albersusa")) %>%
-  add_code_step(devtools::install_github("pat-s/rcmdcheck@catch-test-errors"))
 
 ###
 # deploy pkgdowm site
