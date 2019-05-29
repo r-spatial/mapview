dac64fe236af0248b9edcf27e810c9f57f24279c
tim-salabim <tim.appelhans@gmail.com>
2019-05-29 19:51:15 +0200

    try condensed tic

--- tic.R
+++ tic.R
@@ -3,12 +3,13 @@ add_package_checks(error_on = "error")
 ###
 # deploy pkgdowm site
 ###
-if (Sys.getenv("id_rsa") != "") {
-
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
-}
+# if (Sys.getenv("id_rsa") != "") {
+#
+#   get_stage("before_deploy") %>%
+#     add_step(step_setup_ssh())
+#
+#   get_stage("deploy") %>%
+#     add_step(step_build_pkgdown()) %>%
+#     add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+# }
+do_pkgdown(document = FALSE)
