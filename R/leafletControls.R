0744f1c23d528622d8c7db2e86604de93afba067
tim-salabim <tim.appelhans@gmail.com>
2020-07-04 13:59:35 +0200

    set pane for baselayers

--- R/leafletControls.R
+++ R/leafletControls.R
@@ -225,11 +225,17 @@ initBaseMaps <- function(map.types, canvas = FALSE, viewer.suppress = FALSE) {
   )
   if (!(is.null(map.types))) {
     m <- leaflet::addProviderTiles(m, provider = map.types[1],
-                                   layerId = lid[1], group = map.types[1])
+                                   layerId = lid[1], group = map.types[1],
+                                   options = providerTileOptions(
+                                     pane = "tilePane"
+                                   ))
     if (length(map.types) > 1) {
       for (i in 2:length(map.types)) {
         m <- leaflet::addProviderTiles(m, provider = map.types[i],
-                                       layerId = lid[i], group = map.types[i])
+                                       layerId = lid[i], group = map.types[i],
+                                       options = providerTileOptions(
+                                         pane = "tilePane"
+                                       ))
       }
     }
   }
