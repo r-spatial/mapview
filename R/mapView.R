19c2b201ab0ed22f5e8a399ea6c4c9bc58b283d1
lbusett <lbusett@gmail.com>
2020-07-09 22:22:10 +0200

    Allow rendering and downsampling of stars proxy raster

--- R/mapView.R
+++ R/mapView.R
@@ -295,30 +295,30 @@ setMethod('mapView', signature(x = 'RasterLayer'),
               #             layer.name = layer.name,
               #             ...)
               # } else {
-                leafletRL(x,
-                          map = map,
-                          maxpixels = maxpixels,
-                          col.regions = col.regions,
-                          at = at,
-                          na.color = na.color,
-                          use.layer.names = use.layer.names,
-                          map.types = map.types,
-                          alpha.regions = alpha.regions,
-                          legend = legend,
-                          legend.opacity = legend.opacity,
-                          trim = trim,
-                          verbose = verbose,
-                          layer.name = layer.name,
-                          homebutton = homebutton,
-                          native.crs = native.crs,
-                          method = method,
-                          label = label,
-                          query.type = query.type,
-                          query.digits = query.digits,
-                          query.position = query.position,
-                          query.prefix = query.prefix,
-                          viewer.suppress = viewer.suppress,
-                          ...)
+              leafletRL(x,
+                        map = map,
+                        maxpixels = maxpixels,
+                        col.regions = col.regions,
+                        at = at,
+                        na.color = na.color,
+                        use.layer.names = use.layer.names,
+                        map.types = map.types,
+                        alpha.regions = alpha.regions,
+                        legend = legend,
+                        legend.opacity = legend.opacity,
+                        trim = trim,
+                        verbose = verbose,
+                        layer.name = layer.name,
+                        homebutton = homebutton,
+                        native.crs = native.crs,
+                        method = method,
+                        label = label,
+                        query.type = query.type,
+                        query.digits = query.digits,
+                        query.position = query.position,
+                        query.prefix = query.prefix,
+                        viewer.suppress = viewer.suppress,
+                        ...)
             } else {
               NULL
             }
@@ -331,80 +331,113 @@ setMethod('mapView', signature(x = 'RasterLayer'),
 
 ## Stars layer ==================================================================
 #' @describeIn mapView \code{\link{stars}}
-setMethod('mapView', signature(x = 'stars'),
-          function(x,
-                   band = 1,
-                   map = NULL,
-                   maxpixels = mapviewGetOption("mapview.maxpixels"),
-                   col.regions = mapviewGetOption("raster.palette"),
-                   at = NULL,
-                   na.color = mapviewGetOption("na.color"),
-                   use.layer.names = mapviewGetOption("use.layer.names"),
-                   map.types = mapviewGetOption("basemaps"),
-                   alpha.regions = 0.8,
-                   legend = mapviewGetOption("legend"),
-                   legend.opacity = 1,
-                   trim = mapviewGetOption("trim"),
-                   verbose = mapviewGetOption("verbose"),
-                   layer.name = NULL,
-                   homebutton = mapviewGetOption("homebutton"),
-                   native.crs = mapviewGetOption("native.crs"),
-                   method = mapviewGetOption("method"),
-                   label = TRUE,
-                   query.type = mapviewGetOption("query.type"),
-                   query.digits = mapviewGetOption("query.digits"),
-                   query.position = mapviewGetOption("query.position"),
-                   query.prefix = mapviewGetOption("query.prefix"),
-                   viewer.suppress = mapviewGetOption("viewer.suppress"),
-                   ...) {
+.stars_method <- function(x,
+                          band = 1,
+                          map = NULL,
+                          maxpixels = mapviewGetOption("mapview.maxpixels"),
+                          col.regions = mapviewGetOption("raster.palette"),
+                          at = NULL,
+                          na.color = mapviewGetOption("na.color"),
+                          use.layer.names = mapviewGetOption("use.layer.names"),
+                          map.types = mapviewGetOption("basemaps"),
+                          alpha.regions = 0.8,
+                          legend = mapviewGetOption("legend"),
+                          legend.opacity = 1,
+                          trim = mapviewGetOption("trim"),
+                          verbose = mapviewGetOption("verbose"),
+                          layer.name = NULL,
+                          homebutton = mapviewGetOption("homebutton"),
+                          native.crs = mapviewGetOption("native.crs"),
+                          method = mapviewGetOption("method"),
+                          label = TRUE,
+                          query.type = mapviewGetOption("query.type"),
+                          query.digits = mapviewGetOption("query.digits"),
+                          query.position = mapviewGetOption("query.position"),
+                          query.prefix = mapviewGetOption("query.prefix"),
+                          viewer.suppress = mapviewGetOption("viewer.suppress"),
+                          ...) {
 
-            # method = match.arg(method)
+  # method = match.arg(method)
 
-            if (mapviewGetOption("platform") != "leaflet") {
-              warning(
-                sprintf(
-                  "platform '%s' currently doesn't support stars data."
-                  , mapviewGetOption("platform")
-                )
-                , " switching to platform 'leaflet'"
-                , call. = FALSE
-              )
-              mapviewOptions(platform = "leaflet")
-            }
+  # check if downsampling is needed
+  dims <- dim(x)
+  n_pixels <- dims[1] * dims[2]
+  do.downscale <- if (n_pixels > maxpixels) {
+    TRUE
+  } else {
+    FALSE
+  }
 
-            if (mapviewGetOption("platform") == "leaflet") {
-              leaflet_stars(x,
-                            band = band,
-                            map = map,
-                            maxpixels = maxpixels,
-                            col.regions = col.regions,
-                            at = at,
-                            na.color = na.color,
-                            use.layer.names = use.layer.names,
-                            map.types = map.types,
-                            alpha.regions = alpha.regions,
-                            legend = legend,
-                            legend.opacity = legend.opacity,
-                            trim = trim,
-                            verbose = verbose,
-                            layer.name = layer.name,
-                            homebutton = homebutton,
-                            native.crs = native.crs,
-                            method = method,
-                            label = label,
-                            query.type = query.type,
-                            query.digits = query.digits,
-                            query.position = query.position,
-                            query.prefix = query.prefix,
-                            viewer.suppress = viewer.suppress,
-                            ...)
-            } else {
-              NULL
-            }
+  # convert to proper "stars" if proxy, and downscale if needed
+  if (inherits(x, "stars_proxy")) {
+    if (!do.downscale) {
+      x <-  stars::st_as_stars(x)
+    } else {
+      # compute the resampling factor
+      asp   <- dims[1] / dims[2]
+      y_new <- sqrt(maxpixels / asp)
+      x_new <- y_new * asp
+      downsample <- dims[1] / x_new
+      x <- stars::st_as_stars(x, downsample = downsample - 1)
+      message("Number of pixels is above ", maxpixels, ".",
+              "Only about ", maxpixels, " pixels will be shown.\n",
+              "You can increase the value of `maxpixels` to ", prod(dims), " to avoid this.")
+    }
+  } else {
+    if (do.downscale) {
+      message("Number of pixels is above ", maxpixels, ". ",
+              "Automatic downsampling of `stars` object is not yet implemented, so rendering may be slow.\n",
+              "You can pass a `stars` proxy object to mapview to get automatic downsampling.")
+    }
+  }
 
-          }
-)
 
+  if (mapviewGetOption("platform") != "leaflet") {
+    warning(
+      sprintf(
+        "platform '%s' currently doesn't support stars data."
+        , mapviewGetOption("platform")
+      )
+      , " switching to platform 'leaflet'"
+      , call. = FALSE
+    )
+    mapviewOptions(platform = "leaflet")
+  }
+
+  if (mapviewGetOption("platform") == "leaflet") {
+    leaflet_stars(x,
+                  band = band,
+                  map = map,
+                  maxpixels = maxpixels,
+                  col.regions = col.regions,
+                  at = at,
+                  na.color = na.color,
+                  use.layer.names = use.layer.names,
+                  map.types = map.types,
+                  alpha.regions = alpha.regions,
+                  legend = legend,
+                  legend.opacity = legend.opacity,
+                  trim = trim,
+                  verbose = verbose,
+                  layer.name = layer.name,
+                  homebutton = homebutton,
+                  native.crs = native.crs,
+                  method = method,
+                  label = label,
+                  query.type = query.type,
+                  query.digits = query.digits,
+                  query.position = query.position,
+                  query.prefix = query.prefix,
+                  viewer.suppress = viewer.suppress,
+                  ...)
+  } else {
+    NULL
+  }
+
+}
+
+setMethod('mapView', signature(x = 'stars'), .stars_method)
+setMethod('mapView', signature(x = 'stars_proxy'), .stars_method)
 
 
 ## Raster Stack/Brick ===========================================================
@@ -657,33 +690,33 @@ setMethod('mapView', signature(x = 'sf'),
                       ...)
             } else if (mapviewGetOption("platform") == "leaflet") {
 
-                leaflet_sf(x,
-                           map = map,
-                           pane = pane,
-                           zcol = zcol,
-                           color = color,
-                           col.regions = col.regions,
-                           at = at,
-                           na.color = na.color,
-                           cex = cex,
-                           lwd = lwd,
-                           alpha = alpha,
-                           alpha.regions = alpha.regions,
-                           na.alpha = na.alpha,
-                           map.types = map.types,
-                           verbose = verbose,
-                           popup = popup,
-                           layer.name = layer.name,
-                           label = label,
-                           legend = legend,
-                           legend.opacity = legend.opacity,
-                           homebutton = homebutton,
-                           native.crs = native.crs,
-                           highlight = highlight,
-                           maxpoints = maxpoints,
-                           canvas = canvas,
-                           viewer.suppress = viewer.suppress,
-                           ...)
+              leaflet_sf(x,
+                         map = map,
+                         pane = pane,
+                         zcol = zcol,
+                         color = color,
+                         col.regions = col.regions,
+                         at = at,
+                         na.color = na.color,
+                         cex = cex,
+                         lwd = lwd,
+                         alpha = alpha,
+                         alpha.regions = alpha.regions,
+                         na.alpha = na.alpha,
+                         map.types = map.types,
+                         verbose = verbose,
+                         popup = popup,
+                         layer.name = layer.name,
+                         label = label,
+                         legend = legend,
+                         legend.opacity = legend.opacity,
+                         homebutton = homebutton,
+                         native.crs = native.crs,
+                         highlight = highlight,
+                         maxpoints = maxpoints,
+                         canvas = canvas,
+                         viewer.suppress = viewer.suppress,
+                         ...)
 
             } else if (mapviewGetOption("platform") == "leafgl") {
 
@@ -881,65 +914,65 @@ setMethod('mapView', signature(x = 'sfc'),
 #'
 #' @describeIn mapView \code{\link{character}}
 setMethod('mapView', signature(x = 'character'),
-         function(x,
-                  map = NULL,
-                  tms = TRUE,
-                  color = standardColor(),
-                  col.regions = standardColRegions(),
-                  at = NULL,
-                  na.color = mapviewGetOption("na.color"),
-                  cex = 6,
-                  lwd = 2,
-                  alpha = 0.9,
-                  alpha.regions = 0.6,
-                  na.alpha = 0.6,
-                  map.types = mapviewGetOption("basemaps"),
-                  verbose = FALSE,
-                  layer.name = x,
-                  homebutton = mapviewGetOption("homebutton"),
-                  native.crs = FALSE,
-                  canvas = FALSE,
-                  viewer.suppress = mapviewGetOption("viewer.suppress"),
-                  ...) {
-           if (mapviewGetOption("platform") == "leaflet") {
-             if (utils::file_test("-d", x)) {
-               leaflet_tiles(x = x,
+          function(x,
+                   map = NULL,
+                   tms = TRUE,
+                   color = standardColor(),
+                   col.regions = standardColRegions(),
+                   at = NULL,
+                   na.color = mapviewGetOption("na.color"),
+                   cex = 6,
+                   lwd = 2,
+                   alpha = 0.9,
+                   alpha.regions = 0.6,
+                   na.alpha = 0.6,
+                   map.types = mapviewGetOption("basemaps"),
+                   verbose = FALSE,
+                   layer.name = x,
+                   homebutton = mapviewGetOption("homebutton"),
+                   native.crs = FALSE,
+                   canvas = FALSE,
+                   viewer.suppress = mapviewGetOption("viewer.suppress"),
+                   ...) {
+            if (mapviewGetOption("platform") == "leaflet") {
+              if (utils::file_test("-d", x)) {
+                leaflet_tiles(x = x,
+                              map = map,
+                              tms = tms,
+                              map.types = map.types,
+                              verbose = verbose,
+                              layer.name = layer.name,
+                              homebutton = homebutton,
+                              native.crs = native.crs,
+                              viewer.suppress = viewer.suppress,
+                              ...)
+              } else if (utils::file_test("-f", x)) {
+                layer.name = basename(tools::file_path_sans_ext(layer.name))
+                leaflet_file(x = x,
                              map = map,
-                             tms = tms,
+                             color = color,
+                             col.regions = col.regions,
+                             at = at,
+                             na.color = na.color,
+                             cex = cex,
+                             lwd = lwd,
+                             alpha = alpha,
+                             alpha.regions = alpha.regions,
+                             na.alpha = na.alpha,
                              map.types = map.types,
                              verbose = verbose,
                              layer.name = layer.name,
                              homebutton = homebutton,
                              native.crs = native.crs,
+                             canvas = canvas,
                              viewer.suppress = viewer.suppress,
                              ...)
-             } else if (utils::file_test("-f", x)) {
-               layer.name = basename(tools::file_path_sans_ext(layer.name))
-               leaflet_file(x = x,
-                            map = map,
-                            color = color,
-                            col.regions = col.regions,
-                            at = at,
-                            na.color = na.color,
-                            cex = cex,
-                            lwd = lwd,
-                            alpha = alpha,
-                            alpha.regions = alpha.regions,
-                            na.alpha = na.alpha,
-                            map.types = map.types,
-                            verbose = verbose,
-                            layer.name = layer.name,
-                            homebutton = homebutton,
-                            native.crs = native.crs,
-                            canvas = canvas,
-                            viewer.suppress = viewer.suppress,
-                            ...)
-             } else {
-               stop(sprintf("%s is not a directory!", layer.name),
-                    call. = FALSE)
-             }
-           }
-         }
+              } else {
+                stop(sprintf("%s is not a directory!", layer.name),
+                     call. = FALSE)
+              }
+            }
+          }
 )
 
 ## numeric ================================================================
@@ -1176,7 +1209,7 @@ setMethod('mapView', signature(x = 'missing'),
 #'
 setMethod('mapView', signature(x = 'NULL'),
           function(x, ...) {
-              NULL
+            NULL
           }
 )
 
