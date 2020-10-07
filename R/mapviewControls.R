2fbb31e13a8bbe1c947b246fd4df5aa4ede35f57
tim-salabim <tim.appelhans@gmail.com>
2020-10-07 19:02:09 +0200

    make sure limits are different if all values are equal... addresses #343

--- R/mapviewControls.R
+++ R/mapviewControls.R
@@ -1,5 +1,5 @@
 
-isAvailableInLeaflet <- function() {
+isAvailableInLeaflet = function() {
   return(
     list(
       lab = "label" %in% names(as.list(args(leaflet::addCircleMarkers))),
@@ -9,23 +9,23 @@ isAvailableInLeaflet <- function() {
   )
 }
 
-# lab_avl <- isAvailableInLeaflet()$lab
-# scl_avl <- isAvailableInLeaflet()$scl
+# lab_avl = isAvailableInLeaflet()$lab
+# scl_avl = isAvailableInLeaflet()$scl
 
-warn <- paste("Feature labels on mouseover and 'addScaleBar' are not supported in the installed version of 'leaflet'.",
-              "\nRun devtools::install_github('rstudio/leaflet') and re-install 'mapview' locally to enable these features.")
+warn = paste("Feature labels on mouseover and 'addScaleBar' are not supported in the installed version of 'leaflet'.",
+             "\nRun devtools::install_github('rstudio/leaflet') and re-install 'mapview' locally to enable these features.")
 
 
 
 
 ### mapview to leaflet
-mapview2leaflet <- function(x) {
+mapview2leaflet = function(x) {
   methods::slot(x, "map")
 }
 
 
 ### mapview simple class
-getSimpleClass <- function(obj) {
+getSimpleClass = function(obj) {
   if (inherits(obj, c("RasterLayer", "RasterStack",
                       "RasterBrick", "Satellite",
                       "SpatialGridDataFrame",
@@ -34,31 +34,31 @@ getSimpleClass <- function(obj) {
 
 
 ### labels
-makeLabels <- function(x, zcol = NULL) {
+makeLabels = function(x, zcol = NULL) {
   if (inherits(x, "XY")) {
-    lab <- "1"
+    lab = "1"
   } else if (inherits(x, "sfc")) {
-    lab <- as.character(seq(length(x)))
+    lab = as.character(seq(length(x)))
   } else if (inherits(x, "sf") & is.null(zcol)) {
-    lab <- rownames(x)
+    lab = rownames(x)
   } else if (inherits(x, "Raster")) {
     lab = TRUE
-  } else lab <- as.character(as.data.frame(x)[, zcol])
+  } else lab = as.character(as.data.frame(x)[, zcol])
   return(lab)
 }
 
 
-makeLabelsSP <- function(col) {
+makeLabelsSP = function(col) {
   as.character(col)
 }
 
 
 ### getFeatureIds
-getFeatureIds <- function(att_data) {
+getFeatureIds = function(att_data) {
   if (inherits(att_data, "Spatial") | inherits(att_data, "sf")) {
-    ids <- row.names(att_data)
+    ids = row.names(att_data)
   } else if (inherits(att_data, "sfc")) {
-    ids <- seq(length(att_data))
+    ids = seq(length(att_data))
   }
 
   return(ids)
@@ -66,37 +66,37 @@ getFeatureIds <- function(att_data) {
 
 
 ### createExtent
-createExtent <- function(x, offset = NULL) {
+createExtent = function(x, offset = NULL) {
 
   if (inherits(x, "Extent")) {
     return(x)
   } else {
     if (inherits(x, "Raster")) {
-      ext <- raster::extent(
+      ext = raster::extent(
         raster::projectExtent(x, crs = sp::CRS("+init=epsg:4326")))
     } else if (inherits(x, "Spatial")) {
-      ext <- raster::extent(raster::xmin(x),
-                            raster::xmax(x),
-                            raster::ymin(x),
-                            raster::ymax(x))
+      ext = raster::extent(raster::xmin(x),
+                           raster::xmax(x),
+                           raster::ymin(x),
+                           raster::ymax(x))
     } else if (inherits(x, "sfc") | inherits(x, "sf") |
                inherits(x, "XY") | inherits(x, "stars")) {
-      bb <- sf::st_bbox(x)
-      ext <- raster::extent(bb[1], bb[3], bb[2], bb[4])
+      bb = sf::st_bbox(x)
+      ext = raster::extent(bb[1], bb[3], bb[2], bb[4])
     }
 
     if (is.null(offset)) {
-      xxtend <- c(ext[1], ext[2])
-      yxtend <- c(ext[3], ext[4])
-      ext@xmin <- xxtend[1]
-      ext@xmax <- xxtend[2]
-      ext@ymin <- yxtend[1]
-      ext@ymax <- yxtend[2]
+      xxtend = c(ext[1], ext[2])
+      yxtend = c(ext[3], ext[4])
+      ext@xmin = xxtend[1]
+      ext@xmax = xxtend[2]
+      ext@ymin = yxtend[1]
+      ext@ymax = yxtend[2]
     } else {
-      ext@xmin <- ext@xmin - offset
-      ext@xmax <- ext@xmax + offset
-      ext@ymin <- ext@ymin - offset
-      ext@ymax <- ext@ymax + offset
+      ext@xmin = ext@xmin - offset
+      ext@xmax = ext@xmax + offset
+      ext@ymin = ext@ymin - offset
+      ext@ymax = ext@ymax + offset
     }
 
     return(ext)
@@ -105,7 +105,7 @@ createExtent <- function(x, offset = NULL) {
 }
 
 
-isMultiFeature <- function(x) {
+isMultiFeature = function(x) {
   #stopifnot(inherits(x, "sfg"))
   if (inherits(x, "POINT") |
       inherits(x, "LINESTRING") |
@@ -120,12 +120,12 @@ isMultiFeature <- function(x) {
 
 
 ### getSFClass
-getSFClass <- function(x) {
+getSFClass = function(x) {
   if (class(x)[1] == "XY") class(x)[2] else class(x)[1]
 }
 
 
-getGeometryType <- function(x) {
+getGeometryType = function(x) {
   # raster / stars
   if (inherits(x, c("Raster", "stars"))) {
     return("rs")
@@ -133,26 +133,26 @@ getGeometryType <- function(x) {
 
   # sf
   if (inherits(x, "Spatial")) x = sf::st_as_sfc(x)
-  g <- sf::st_geometry(x)
+  g = sf::st_geometry(x)
   if (inherits(g, "POINT") |
       inherits(g, "MULTIPOINT") |
       inherits(g, "sfc_POINT") |
-      inherits(g, "sfc_MULTIPOINT")) type <- "pt"
+      inherits(g, "sfc_MULTIPOINT")) type = "pt"
   if (inherits(g, "LINESTRING") |
       inherits(g, "MULTILINESTRING") |
       inherits(g, "sfc_LINESTRING") |
-      inherits(g, "sfc_MULTILINESTRING")) type <- "ln"
+      inherits(g, "sfc_MULTILINESTRING")) type = "ln"
   if (inherits(g, "POLYGON") |
       inherits(g, "MULTIPOLYGON") |
       inherits(g, "sfc_POLYGON") |
-      inherits(g, "sfc_MULTIPOLYGON")) type <- "pl"
+      inherits(g, "sfc_MULTIPOLYGON")) type = "pl"
   if (inherits(g, "sfc_GEOMETRY") |
-      inherits(g, "sfc_GEOMETRYCOLLECTION")) type <- "gc" #getGeometryType(sf::st_cast(g))
+      inherits(g, "sfc_GEOMETRYCOLLECTION")) type = "gc" #getGeometryType(sf::st_cast(g))
   return(type)
 }
 
 
-getMaxFeatures <- function(x) {
+getMaxFeatures = function(x) {
   switch(getGeometryType(x),
          "pt" = 40000,
          "ln" = 300000,
@@ -161,7 +161,7 @@ getMaxFeatures <- function(x) {
 }
 
 
-lineWidth <- function(x) {
+lineWidth = function(x) {
   lw = switch(getGeometryType(x),
               "pt" = 1,
               "ln" = 2,
@@ -171,7 +171,7 @@ lineWidth <- function(x) {
 }
 
 
-regionOpacity <- function(x) {
+regionOpacity = function(x) {
   switch(
     getGeometryType(x)
     , "pt" = ifelse(mapviewGetOption("platform") == "leafgl", 0.8, 0.6)
@@ -183,8 +183,8 @@ regionOpacity <- function(x) {
 }
 
 
-basemaps <- function(colors) {
-  ml <- mean(as.numeric(sapply(colors, luminence)))
+basemaps = function(colors) {
+  ml = mean(as.numeric(sapply(colors, luminence)))
   if (length(unique(colors)) == 1) {
     unique_cyan = ifelse(unique(colors) %in% c("cyan", "#00ffff", "#00FFFF"), TRUE, FALSE)
   } else {
@@ -198,12 +198,12 @@ basemaps <- function(colors) {
 }
 
 
-getProjection <- function(x) {
+getProjection = function(x) {
 
   if (inherits(x, c("Raster", "Spatial"))) {
-    prj <- raster::projection(x)
+    prj = raster::projection(x)
   } else {
-    prj <- sf::st_crs(x)$proj4string
+    prj = sf::st_crs(x)$proj4string
   }
 
   return(prj)
@@ -211,39 +211,40 @@ getProjection <- function(x) {
 }
 
 
-createFileId <- function(ndigits = 6) {
+createFileId = function(ndigits = 6) {
   paste(sample(c(letters[1:6], 0:9), ndigits), collapse = "")
 }
 
 
-extendLimits <- function(lim, length = 1, prop = 0.07) {
-  if(length(lim) != 2) stop("lim should be of length 2")
-  if(lim[1] > lim[2]) lim <- rev(lim)
+extendLimits = function(lim, length = 1, prop = 0.07) {
+  if (length(lim) != 2) stop("lim should be of length 2")
+  if (lim[1] > lim[2]) lim = rev(lim)
+  if (all.equal(lim[1], lim[2])) lim = lim * c(0.999, 1.001)
   if (!missing(length)) {
-    prop <- (as.numeric(length) -
-               as.numeric(diff(lim))) / (2 * as.numeric(diff(lim)))
+    prop = (as.numeric(length) -
+              as.numeric(diff(lim))) / (2 * as.numeric(diff(lim)))
   }
   if (lim[1] == lim[2])
     lim + 0.005 * c(-length, length)
   else {
-    d <- diff(as.numeric(lim))
+    d = diff(as.numeric(lim))
     lim + prop * d * c(-1, 1)
   }
 }
 
 
-circleRadius <- function(x, radius = 6, min.rad = 3, max.rad = 15, na.rad = 2, ...) {
+circleRadius = function(x, radius = 6, min.rad = 3, max.rad = 15, na.rad = 2, ...) {
 
   if (is.character(radius)) {
-    rad <- scales::rescale(as.numeric(x[[radius]]),
-                           to = c(min.rad, max.rad))
+    rad = scales::rescale(as.numeric(x[[radius]]),
+                          to = c(min.rad, max.rad))
     rad[is.na(rad)] = na.rad
-  } else rad <- radius
+  } else rad = radius
   return(rad)
 }
 
 
-extentOverlap <- function(x, y) {
+extentOverlap = function(x, y) {
   if (!sum(lengths(sf::st_intersects(x, y))) == 0) TRUE else FALSE
 }
 
