25326e514b44454c0084b004464033252220f34e
tim-salabim <tim.appelhans@gmail.com>
2020-11-21 12:19:52 +0100

    use standard htmlwidget printing when fgb & georaster options are FALSE. close #351

--- R/print.R
+++ R/print.R
@@ -1,50 +1,59 @@
+printMapview = function (x) {
+
+  ## normal htmlwidget printing for notebooks etc.
+  ## set options fgb & georaster to FALSE!!
+  if (!isTRUE(mapviewGetOption("fgb")) & !isTRUE(mapviewGetOption("georaster"))) {
+    print(mapview2leaflet(x))
+    return(invisible(x))
+  }
+
+  x = x@map
+  viewer = getOption("viewer")
+  if (mapviewGetOption("viewer.suppress")) {
+    viewer = NULL
+  }
+  if (!is.null(viewer)) {
+    viewerFunc = function(url) {
+      paneHeight = x$sizingPolicy$viewer$paneHeight
+      if (identical(paneHeight, "maximize")) {
+        paneHeight = -1
+      }
+      viewer(url, height = paneHeight)
+    }
+  } else {
+    viewerFunc = function(url) {
+      dir = gsub("file://|/index.html", "", url)
+      rs = requireNamespace("rstudioapi", quietly = TRUE)
+      if (rs) {
+        if (mapviewGetOption("viewer.suppress") &
+            rstudioapi::isAvailable()) {
+          fl = file.path(dir, "index.html")
+          utils::browseURL(fl)
+        } else {
+          servr::httd(
+            dir = dir
+            , verbose = FALSE
+          )
+        }
+      } else {
+        servr::httd(
+          dir = dir
+          , verbose = FALSE
+        )
+      }
+    }
+  }
+  htmltools::html_print(
+    htmltools::as.tags(x, standalone = TRUE)
+    , viewer = if (interactive()) viewerFunc
+  )
+  invisible(x)
+}
+
 #' Method for printing mapview objects
 #' @param x a mapview object
-setMethod('print', signature(x = "mapview"),
-          function (x)
-          {
-            x = x@map
-            viewer = getOption("viewer")
-            if (mapviewGetOption("viewer.suppress")) {
-              viewer = NULL
-            }
-            if (!is.null(viewer)) {
-              viewerFunc = function(url) {
-                paneHeight = x$sizingPolicy$viewer$paneHeight
-                if (identical(paneHeight, "maximize"))
-                  paneHeight = -1
-                viewer(url, height = paneHeight)
-              }
-            } else {
-              viewerFunc = function(url) {
-                dir = gsub("file://|/index.html", "", url)
-                rs = requireNamespace("rstudioapi", quietly = TRUE)
-                if (rs) {
-                  if (mapviewGetOption("viewer.suppress") &
-                      rstudioapi::isAvailable()) {
-                    fl = file.path(dir, "index.html")
-                    utils::browseURL(fl)
-                  } else {
-                    servr::httd(
-                      dir = dir
-                      , verbose = FALSE
-                    )
-                  }
-                } else {
-                  servr::httd(
-                    dir = dir
-                    , verbose = FALSE
-                  )
-                }
-              }
-            }
-            htmltools::html_print(
-              htmltools::as.tags(x, standalone = TRUE)
-              , viewer = if (interactive()) viewerFunc
-            )
-            invisible(x)
-          }
-)
+#'
+setMethod('print', signature(x = "mapview"), printMapview)
 
 #' Method for printing mapview objects (show)
 #' @param object a mapview object
