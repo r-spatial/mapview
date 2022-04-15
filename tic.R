cacfd6cc9ab11d7fa20f327f346835a9259d9677
tim-salabim <tim.appelhans@gmail.com>
2022-04-15 13:07:10 +0200

    try --resave-data in tic

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
+do_package_checks(build_args = "--resave-data")
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
