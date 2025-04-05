7ca1402edd3241046fd57b982d1eec1c9927377b
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-04-05 15:42:40 +1300

    refactor: added helper functions and switch between them

--- R/mapgl.R
+++ R/mapgl.R
@@ -26,6 +26,8 @@ mapgl_sf <- function(
   ...
 ) {
   # Initialize map centered on data
+  # TODO: Add switch for mapgl::mapboxgl depending on
+  # mapviewOptions("mapgl.platform") or similar
   m <- mapgl::maplibre(bounds = x)
 
   # Add source
@@ -50,7 +52,11 @@ mapgl_sf <- function(
     if (!is.null(color)) {
       color <- ifelse(is.function(color), standardColor(x), color)
     }
-    col.regions <- ifelse(is.function(col.regions), standardColRegions(x), col.regions)
+    col.regions <- ifelse(
+      is.function(col.regions),
+      standardColRegions(x),
+      col.regions
+    )
   }
 
   # Handle alpha inheritance
@@ -58,54 +64,64 @@ mapgl_sf <- function(
     na.alpha <- ifelse(na.alpha == 0, 0.001, na.alpha)
     if (length(alpha) != nrow(x)) alpha <- rep(alpha, nrow(x))
     alpha[is.na(x[[zcol]])] <- na.alpha
-    if (length(alpha.regions) != nrow(x)) alpha.regions <- rep(alpha.regions, nrow(x))
+    if (length(alpha.regions) != nrow(x))
+      alpha.regions <- rep(alpha.regions, nrow(x))
     alpha.regions[is.na(x[[zcol]])] <- na.alpha
   }
 
   # Add appropriate layers based on geometry type
   # TODO: Add support for "gc" and "rs"
-  if (geom_type == "pl") {
-    m <-
-      mapgl::add_fill_layer(
-        map = m,
-        id = "data_fill",
-        source = "data",
-        fill_color = col.regions,
-        fill_opacity = alpha.regions
-      ) |>
-      mapgl::add_line_layer(
-        id = "data_line",
-        source = "data",
-        line_color = color,
-        line_width = lwd,
-        line_opacity = alpha
-      )
-  } else if (geom_type == "ln") {
-    m <-
-      mapgl::add_line_layer(
-        map = m,
-        id = "data_line",
-        source = "data",
-        line_color = color,
-        line_width = lwd,
-        line_opacity = alpha
-      )
-  } else if (geom_type == "pt") {
-    m <-
-      mapgl::add_circle_layer(
-        map = m,
-        id = "data_point",
-        source = "data",
-        circle_opacity = alpha.regions,
-        circle_color = col.regions,
-        circle_stroke_color = color,
-        circle_stroke_width = lwd,
-        circle_radius = cex
-      )
-  }
+  m <- switch(
+    geom_type,
+    "pl" = mapgl_pl(m, col.regions, alpha.regions, color, lwd, alpha),
+    "ln" = mapgl_ln(m, color, lwd, alpha),
+    "pt" = mapgl_pt(m, alpha.regions, col.regions, color, lwd, cex)
+  )
 
   # Add navigation control
   m <- mapgl::add_navigation_control(m)
 
   return(m)
+}
+
+# Helper functions for each geometry type
+mapgl_pl <- function(m, col.regions, alpha.regions, color, lwd, alpha) {
+  mapgl::add_fill_layer(
+    map = m,
+    id = "data_fill",
+    source = "data",
+    fill_color = col.regions,
+    fill_opacity = alpha.regions
+  ) |>
+    mapgl::add_line_layer(
+      id = "data_line",
+      source = "data",
+      line_color = color,
+      line_width = lwd,
+      line_opacity = alpha
+    )
+}
+
+mapgl_ln <- function(m, color, lwd, alpha) {
+  mapgl::add_line_layer(
+    map = m,
+    id = "data_line",
+    source = "data",
+    line_color = color,
+    line_width = lwd,
+    line_opacity = alpha
+  )
+}
+
+mapgl_pt <- function(m, alpha.regions, col.regions, color, lwd, cex) {
+  mapgl::add_circle_layer(
+    map = m,
+    id = "data_point",
+    source = "data",
+    circle_opacity = alpha.regions,
+    circle_color = col.regions,
+    circle_stroke_color = color,
+    circle_stroke_width = lwd,
+    circle_radius = cex
+  )
 }
